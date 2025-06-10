0797ea717084d9ad489998cbaaf4d0220e54f196
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-10 10:41:48 -0700

    implement toggle day or night

--- src/components/02-Toggle.jsx
+++ src/components/02-Toggle.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+
 export default function Toggle() {
+  const [timeToggle, setTimeToggle] = useState(false)
+  const onToggle = () => setTimeToggle(!timeToggle)
+  const onDay = () => setTimeToggle(true)
+  const onNight = () => setTimeToggle(false)
+
   return (
     <div className="App">
       <h2>Toggle</h2>
-      <div className="emoji">💡 🌌</div>
+      <div className="emoji">{timeToggle ? '💡': '🌌'}</div>
       <div className="button-group">
-        <button>Day</button>
-        <button>Night</button>
-        <button>Toggle</button>
+        <button onClick={onDay}>Day</button>
+        <button onClick={onNight}>Night</button>
+        <button onClick={onToggle}>Toggle</button>
       </div>
     </div>
   )
