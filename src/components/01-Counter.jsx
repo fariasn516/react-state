70afd0f7d59d6cd8b0a3ac0830fef411cb328227
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-10 10:25:54 -0700

    add counter state

--- src/components/01-Counter.jsx
+++ src/components/01-Counter.jsx
@@ -1,11 +1,23 @@
+import React, { useState, useEffect } from "react";
+
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div>
       <h2>Counter</h2>
-      <h3>7</h3>
+      <h3>{count}</h3>
       <div className="button-group">
-        <button>Increase</button>
-        <button>Decrease</button>
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
       </div>
     </div>
   )
